ad68222afd828bce55687f6c9b2763125de2aab3
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-01-29 06:22:00 +0000

    [Add]4-12

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -1,10 +1,26 @@
 class PostimagesController < ApplicationController
   def new
+    @postimage = Postimage.new
+  end
+
+  def create
+    @postimage = Postimage.new(postimage_params)
+    @postimage.user_id = current_user.id
+    @postimage.save
+    redirect_to postimages_path
   end
 
   def index
+    @postimages = Postimage.all
   end
 
   def show
+    @postimage = Postimage.find(params[:id])
+  end
+
+  private
+
+  def postimage_params 
+    params.require(:postimage).permit(:shop_name, :image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :postimages, only: [:new, :index, :show]
+  resources :postimages, only: [:new, :index, :show, :create]
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 end
+ 
