f975c85cbd2c0bcb3f13fe016adc3403bc02e980
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-01-30 04:10:11 +0000

    [Add]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit]
   get 'show/edit'
   resources :postimages, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
