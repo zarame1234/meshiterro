3fceb005bb3dd2fe733234ba95a176381def7eba
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-01 00:43:17 +0000

    [Add]4-20

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,6 +1,6 @@
 class PostCommentsController < ApplicationController
 def create
-  postimage = PostImage.find(params[:post_image_id])
+  postimage = PostImage.find(params[:postimage_id])
   comment = current_user.post_comments.new(post_comment_params)
   comment.postimage_id = postimage.id
   comment.save
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :postimage
 
-  validates :user_id, unqueness: {scope: :postimage_id}
+  validates :user_id, uniqueness: {scope: :postimage_id}
 end
