911f689c6581c696ea5cdf11d6043eac1371d168
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-04 01:53:33 +0000

    [Add]4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,32 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @postimages = @user.postimages.page(params[:page])
   end
 
   def edit
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to postimages_path
-    end
-
     @user = User.find(params[:id])
     
   end
 
   def update
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to postimages_path
-    end
-
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
+  private
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to postimages_path
+    end
+  end
+
 end
